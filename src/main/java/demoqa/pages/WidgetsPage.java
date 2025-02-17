e843cd78c0e94ef4ea6eedaea85b2112d920dc31
IgorDerk <derk.i26252@gmail.com>
2025-02-17 21:59:09 +0100

    first commit

---
+++ src/main/java/demoqa/pages/WidgetsPage.java
@@ -0,0 +1,24 @@
+package demoqa.pages;
+
+import demoqa.core.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WidgetsPage extends BasePage {
+    public WidgetsPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    @FindBy(id = "oldSelectMenu")
+    WebElement oldSelectMenu;
+
+    public WidgetsPage selectOldStyleMenu(String color) {
+        Select select = new Select(oldSelectMenu);
+        scrollTo(500);
+        select.selectByVisibleText(color);
+        return this;
+    }
+}
